b36e60dcb5865ab5ef9250b5d28215c864b81f70
smx <744926532@qq.com>
2015-01-01 14:00:14 +0800

    git竟然是非要写message的＝。＝ so ui第一发~

---
+++ src/presentation/home/GuideLabel.java
@@ -0,0 +1,15 @@
+package presentation.home;
+
+import java.awt.Color;
+
+import javax.swing.*;
+
+public class GuideLabel extends JLabel {
+	
+		public GuideLabel(){
+//			this.setSize(150, 150);
+			this.setBackground(Color.orange);
+			this.setOpaque(true);
+		}
+		
+}
