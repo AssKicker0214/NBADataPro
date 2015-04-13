ed081012d3fa1dea49fbec2ed7f8d364d8a77579
chenghao2262 <chenghao2262@gmail.com>
2015-04-13 09:03:54 +0800

    文件处理类

--- src/TEst.java
+++ src/TEst.java
@@ -1,14 +1,26 @@
 import DBmanage.InitialDatabase;
+import fileinput.PlayerFileInput;
+import fileinput.TeamFileInput;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ArrayListHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class TEst {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
         InitialDatabase.initial();
-        InitialDatabase.getConnection();
-        InitialDatabase.getConnection();
-        InitialDatabase.getConnection();
-        InitialDatabase.getConnection();
-        InitialDatabase.getConnection();
-        InitialDatabase.getConnection();
-        InitialDatabase.getConnection();
+        new TeamFileInput().ReadTeam();
+        Connection connection = InitialDatabase.getConnection();
+        QueryRunner queryRunner = new QueryRunner();
+        List<Object[]> arraylist = queryRunner
+                .query(connection, "select * from team ",
+                        new ArrayListHandler());
+        for (int i = 0; i < arraylist.size(); i++){
+            Object[] objects = arraylist.get(i);
+            System.out.println(objects[0]+":"+objects[1]+":"+objects[2]+":"+objects[3]+":"+objects[4]+":"+objects[5]+":"+objects[6]+":"+objects[7]+":");
+        }
+        new PlayerFileInput().readPlayer();
 	}
 }
--- src/fileinput/PositionInput.java
+++ src/fileinput/PositionInput.java
@@ -1,10 +1,14 @@
 package fileinput;
 
+import org.apache.commons.dbutils.QueryRunner;
+
+import java.sql.Connection;
+
 /**
  * Created by chenghao on 15/4/12.
  */
 public class PositionInput {
-    public static void readPosition(){
+    public static void readPosition(Connection connection,QueryRunner queryRunner,String position){
 
     }
 }
