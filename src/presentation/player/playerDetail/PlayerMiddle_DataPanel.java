d7c66afd731fa4bd4e8332e5801c0c4084f26281
smx <744926532@qq.com>
2015-05-03 22:16:26 +0800

    合并赛季数据和球员联盟平均值对比到datapanel

--- src/presentation/player/playerDetail/PlayerMiddle_DataPanel.java
+++ src/presentation/player/playerDetail/PlayerMiddle_DataPanel.java
@@ -1,5 +1,9 @@
 package presentation.player.playerDetail;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 //资料
@@ -10,5 +14,27 @@ public class PlayerMiddle_DataPanel extends JPanel{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-
+	PlayerMiddle_ContrastLeaguePanel contrastPanel;
+	PlayerMiddle_SeasonDataPanel seasonDataPanel;
+	
+	public PlayerMiddle_DataPanel(){
+		this.setLayout(null);
+		this.setBounds(0, 250,1280,400);
+		this.setBackground(Color.WHITE);
+//		setContrastPanel();
+//		setSeasonPanel();
+	}
+	
+	public void setContrastPanel(ArrayList<Double> player,ArrayList<Double> leagueAvg){
+		contrastPanel = new PlayerMiddle_ContrastLeaguePanel(player,leagueAvg);
+		this.add(contrastPanel);
+		repaint();
+	}
+	
+	public void setSeasonPanel(ArrayList<String> avg,ArrayList<String> total){
+		seasonDataPanel = new PlayerMiddle_SeasonDataPanel(avg, total);
+		this.add(seasonDataPanel);
+		repaint();
+	}
+	
 }
--- src/presentation/player/playerDetail/PlayerMiddle_Recent5Match.java
+++ src/presentation/player/playerDetail/PlayerMiddle_Recent5Match.java
@@ -4,48 +4,52 @@ import java.awt.Color;
 import java.awt.Font;
 import java.util.ArrayList;
 
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import presentation.table.TablePane;
 
-
-public class PlayerMiddle_SeasonDataPanel extends JPanel {
+public class PlayerMiddle_Recent5Match extends JPanel{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	JLabel seasonDataTitleLabel;
+	JLabel Recent5MatchLabel;
 	
 	JLabel tabelLabel;
+
 	
-	public PlayerMiddle_SeasonDataPanel(ArrayList<String> avg,ArrayList<String> total){
+	public PlayerMiddle_Recent5Match(){
 		this.setLayout(null);
 		this.setBounds(0, 430, 1280,220);
-		setSeasonDataTitleLabel();
+		setRecent5MactchTitleLabel();
 		this.setBackground(Color.WHITE);
-		setTabel(avg,total);
+		setTabel();
 	}
 	
-	public void setSeasonDataTitleLabel(){
-		seasonDataTitleLabel = new JLabel("   赛季数据",JLabel.LEADING);
-		seasonDataTitleLabel.setFont(new Font("Dialog",1,20));
-		seasonDataTitleLabel.setForeground(Color.WHITE);
+	public void setRecent5MactchTitleLabel(){
+		Recent5MatchLabel = new JLabel("   赛季数据",JLabel.LEADING);
+		Recent5MatchLabel.setFont(new Font("Dialog",1,20));
+		Recent5MatchLabel.setForeground(Color.WHITE);
 //		seasonDataTitleLabel.setBackground(new Color(33,82,138));
-		seasonDataTitleLabel.setBackground(Color.DARK_GRAY);
-		seasonDataTitleLabel.setOpaque(true);
-		seasonDataTitleLabel.setBounds(0,0,1280,50);
-		this.add(seasonDataTitleLabel);
+		Recent5MatchLabel.setBackground(Color.DARK_GRAY);
+		Recent5MatchLabel.setOpaque(true);
+		Recent5MatchLabel.setBounds(0,0,1280,50);
+		this.add(Recent5MatchLabel);
 	}
-	
-	
-	
-	public void setTabel(ArrayList<String> avg,ArrayList<String> total ){
+
+	public void setTabel(){
 		String[] columns = {"年度","球队","场数","先发","分钟","％","三分％","罚球％",
 				"进攻","防守","场均篮板","场均助攻","场均抢断","场均盖帽","失误","犯规","场均得分"};
+		ArrayList<String> items = new ArrayList<String>();
+		items.add("年度"); items.add("球队");items.add("场数");
+		items.add("先发"); items.add("分钟");items.add("％");
+		items.add("三分％"); items.add("罚球％"); items.add("进攻");
+		items.add("防守"); items.add("场均篮板"); items.add("场均助攻");
+		items.add("场均抢断"); items.add("场均盖帽"); items.add("失误");
+		items.add("犯规"); items.add("场均得分");
 		
 		ArrayList<String> seasonAvgItem = new ArrayList<String>();
 		seasonAvgItem.add("赛季平均"); 
@@ -73,23 +77,6 @@ public class PlayerMiddle_SeasonDataPanel extends JPanel {
 		TablePane t = new TablePane(a,columns,w,0,50,1280,200,30,false,false);
 		this.add(t);
 	}
-	
-	public static void main(String[] args){
-		JFrame jf = new JFrame();
-		jf.setLayout(null);
-		jf.setSize(1280,700);
-		jf.setLocationRelativeTo(null);
-		
-		ArrayList<String> avg = new ArrayList<String>();
-		ArrayList<String> total = new ArrayList<String>();
-		
-		for(int i = 0; i < 17;i++){
-			avg.add("8");
-			total.add("10");
-		}
-		jf.add(new PlayerMiddle_SeasonDataPanel(avg,total));
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setVisible(true);
-	}
+
 
 }
