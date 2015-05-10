75d3327c4bca29ade6beeb61869789077855edc5
chenghao2262 <chenghao2262@gmail.com>
2015-05-10 16:12:05 +0800

    Merge branch 'master15' of git.oschina.net:chenghao2262/NBADataPro into master14
    # Conflicts:
    #	src/conf.xml

--- src/configure/Configure.java
+++ src/configure/Configure.java
@@ -10,6 +10,8 @@ import java.net.URLDecoder;
  * Created by chenghao on 15/5/5.
  */
 public class Configure {
+    private static String path;
+
     public static String getTeamPath() {
         String rootPath = getRootPath();
         return rootPath + "/teams/teams";
@@ -24,23 +26,24 @@ public class Configure {
     }
 
     public static void set(String path) {
-        BufferedWriter bw = null;
-        try {
-            bw = new BufferedWriter(new OutputStreamWriter((new FileOutputStream(new File(getRoot() + "conf/conf.xml")))));
-            bw.write(path);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            if (bw != null) {
-                try {
-                    bw.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+//        BufferedWriter bw = null;
+//        try {
+//            bw = new BufferedWriter(new OutputStreamWriter((new FileOutputStream(new File(getRoot() + "conf/conf.xml")))));
+//            bw.write(path);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }finally {
+//            if (bw != null) {
+//                try {
+//                    bw.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+        Configure.path = path;
     }
 
     private static String getConfigureDocument() {
@@ -48,27 +51,28 @@ public class Configure {
     }
 
     private static String getRootPath() {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(getRoot() + "conf/conf.xml"))));
-            String path = br.readLine();
-            if (path != null){
-                return path;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return "";
+        return path;
+//        BufferedReader br = null;
+//        try {
+//            br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(getRoot() + "conf/conf.xml"))));
+//            String path = br.readLine();
+//            if (path != null){
+//                return path;
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }finally {
+//            if (br != null) {
+//                try {
+//                    br.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        return "";
     }
 
     private static String getRoot(){
