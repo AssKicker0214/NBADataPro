b245f4ace667fc9f8ec241b177fe05706ec82556
Galleythrey <njutangshanshan@outlook.com>
2015-05-10 23:29:11 +0800

    ..

--- src/autotest/Main.java
+++ src/autotest/Main.java
@@ -7,22 +7,22 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		Console console = new Console();
 		console.execute(System.out, args);
-//
-//		console.execute(System.out, new String[] {"--datasource","F:/软件工程与计算3/迭代1/迭代一数据"});
-////		console.execute(System.out, new String[] {"-player","-sort","numOfGame.desc"});
-////		console.execute(System.out, new String[] {"-player","-all","-n","10"});
-////		console.execute(System.out, new String[] {"-player","-high","-n","10","-sort","frequency.desc"});
-////		console.execute(System.out, new String[] {"-player","-hot","assist","-n","5"});
-////		console.execute(System.out, new String[] {"-player","-king","score","-season"});
-////		console.execute(System.out, new String[] {"-player","-avg","-n","5","-filter","position.F"});
-////		console.execute(System.out, new String[] {"-player","-total","-all","-n","10","-filter","position.F,league.west","-sort","shot.desc"});
-////		console.execute(System.out, new String[] {"-team"});
-//		console.execute(System.out, new String[] {"-team","-all","-total"});
-////		console.execute(System.out, new String[] {"-team","-all","-n","10"});
-////		console.execute(System.out, new String[] {"-team","-hot","assist","-n","5"});
-////		console.execute(System.out, new String[] {"-team","-avg","-n","5","-sort","shot.asc"});
-////		console.execute(System.out, new String[] {"-team","-total","-all","-n","10","-sort","shot.desc"});
-////		console.execute(System.out, new String[] {"-team","-high","-n","5","-sort","stealEfficient.asc"});
+
+/*		console.execute(System.out, new String[] {"--datasource","F:/软件工程与计算3/迭代1/迭代一数据"});
+		console.execute(System.out, new String[] {"-player","-sort","numOfGame.desc"});
+		console.execute(System.out, new String[] {"-player","-all","-n","10"});
+		console.execute(System.out, new String[] {"-player","-high","-n","10","-sort","frequency.desc"});
+		console.execute(System.out, new String[] {"-player","-hot","assist","-n","5"});
+		console.execute(System.out, new String[] {"-player","-king","score","-season"});
+		console.execute(System.out, new String[] {"-player","-avg","-n","5","-filter","position.F"});
+		console.execute(System.out, new String[] {"-player","-total","-all","-n","10","-filter","position.F,league.west","-sort","shot.desc"});
+		console.execute(System.out, new String[] {"-team"});
+		console.execute(System.out, new String[] {"-team","-all","-total"});
+		console.execute(System.out, new String[] {"-team","-all","-n","10"});
+		console.execute(System.out, new String[] {"-team","-hot","assist","-n","5"});
+		console.execute(System.out, new String[] {"-team","-avg","-n","5","-sort","shot.asc"});
+		console.execute(System.out, new String[] {"-team","-total","-all","-n","10","-sort","shot.desc"});
+		console.execute(System.out, new String[] {"-team","-high","-n","5","-sort","stealEfficient.asc"});*/
 
 	}
 
